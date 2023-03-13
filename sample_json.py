29587db67af354217d4793044ba3489e79f7854f
jdsales1011 <jydeesales@gmail.com>
2023-03-13 10:11:41 +0800

    Added sample_json file.

---
+++ sample_json.py
@@ -0,0 +1,59 @@
+# IDENTIFICATION
+data = {
+    1: {
+        "question": "Zaphod Beeblebrox?",
+        "answer": "Betelgeusian"
+    },
+    2: {
+        "question": "Zaphod Beeblebrox?",
+        "answer": "Betelgeusian"
+    }
+}
+
+# TRUE OR FALSE
+data = {
+    1: {
+        "question": "Zaphod Beeblebrox",
+        "answer": True
+    },
+    2: {
+        "question": "Zaphod Beeblebrox",
+        "answer": False
+    }
+}
+
+# MULTIPLE CHOICE
+data = {
+    1: {
+        "question": "Zaphod Beeblebrox?",
+        "choices": {
+            'a': "the",
+            'b': "quick",
+            'c': "brown",
+            'd': "fox",
+        },
+        "answer": 'd'
+    },
+    2: {
+        "question": "Zaphod Beeblebrox?",
+        "choices": {
+            'a': "the",
+            'b': "quick",
+            'c': "brown",
+            'd': "fox",
+        },
+        "answer": 'd'
+    }
+}
+
+# ESSAY
+data = {
+    1: {
+        "question": "Zaphod Beeblebrox?",
+        "answer": "Lorem ipsum"
+    },
+    2: {
+        "question": "Zaphod Beeblebrox?",
+        "answer": "Lorem ipsum"
+    }
+}
