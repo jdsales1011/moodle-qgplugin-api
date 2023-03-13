0fbc0c60a52ed19e50610d9fb976858a025b9d99
jdsales1011 <jydeesales@gmail.com>
2023-03-13 10:09:58 +0800

    Converted Q-A pairs into dictionary. Response in json format. Todo: Get q_type and number inputs from PHP.

--- moodle_python_api.py
+++ moodle_python_api.py
@@ -1,19 +1,39 @@
+from flask import Flask
+from flask import request
+
 import sys
 import os
+import json
 
 import openai
 
 os.environ['OPENAI_API_KEY']='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'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# context = sys.argv[1]
-# number = int (sys.argv[2])
-# q_type = int(sys.argv[3])
-number = 3
-q_type = 2
-context = '''
-Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary.
-'''
+
+app = Flask(__name__)
+@app.route("/qgplugin/api/", methods = ["POST"])
+def get_questions():
+    # context = sys.argv[1]
+    # number = int (sys.argv[2])
+    # q_type = int(sys.argv[3])
+    number = 3
+    q_type = 2
+    context = '''
+    Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary.
+    '''
+    prompt_creators = {
+        1: prompt_creator1(context, number),
+        2: prompt_creator2(context, number),
+        3: prompt_creator3(context, number),
+        4: prompt_creator4(context, number),
+    }
+    prompt = prompt_creators[q_type]
+    print(prompt)
+
+    # PREDICT FUNCTION
+    return predict_questions(prompt, q_type, number)
+
 
 
 def prompt_creator1(context, num=1): #identification
@@ -28,47 +48,57 @@ def prompt_creator3(context, num=1): #multiple choice
 def prompt_creator4(context, num=1): #essay
     return f"Generate {num} essay questions and the key answers as a list from the following text, {context} using the format 'Q1.' and 'Answer:'\n\nQuestions and Answers:"
 
-prompt_creators = {
-    1: prompt_creator1(context, number),
-    2: prompt_creator2(context, number),
-    3: prompt_creator3(context, number),
-    4: prompt_creator4(context, number),
-}
-
 
 # AI PREDICT FUNCTION
-def predict_questions(prompt):
-  result =[]
-  openai.api_key = os.getenv("OPENAI_API_KEY")
+def predict_questions(prompt, q_type, number):
+    result = []
+    # openai.api_key = os.getenv("OPENAI_API_KEY")    
+    # response = openai.Completion.create(
+    #   model="text-davinci-003",
+    #   prompt=prompt,
+    #   temperature=0.7,
+    #   max_tokens=256,
+    #   top_p=1,
+    #   frequency_penalty=0,
+    #   presence_penalty=0
+    # )   
+    # new = response['choices'][0]['text']
 
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
+    new = '''
+    Q1. The school has a Catholic character?
+    Answer: True 
 
-  new = response['choices'][0]['text']
-#   result = new.split('\n')
-  
+    Q2. Is there a golden statue of the Virgin Mary atop the Main Building's gold dome?
+    Answer: True
 
-#   result = [x for x in result if x.strip()]       # TO REMOVE EMPTY NEXT LINES
+    Q3. Is the Main Building's gold dome the only architectural feature with a religious character?
+    Answer: False
+    '''
 
-  # for index, data in enumerate(result):
-  #   if index%2 == 0:          # QUESTION
-  #     result[index] = data.split(".", 1)[1].strip()
-  #   else:                     # ANSWER
-  #     result[index] = data.split("Answer:", 1)[1].strip()
+    result = new.split('\n')
 
-#   return result
-  return new
+    ques_bank = {}
+
+    if q_type == 3:
+        print("NaN")
+    else:
+        result = [x for x in result if x.strip()]       # TO REMOVE EMPTY NEXT LINES    
+        
+        for i in range(0, number*2, 2):
+            item = {
+                "question" : result[i].split(".", 1)[1].strip(),
+                "answer" : result[i+1].split("Answer:", 1)[1].strip()
+                }
+            ques_bank[int(i/2)+1] = item
+
+    # Convert dict to JSON
+    ques_bank_json = json.dumps(ques_bank)
+    # print(ques_bank_json)
+
+    # Return a JSON 
+    return ques_bank_json
+    # return new
 
 
-prompt = prompt_creators[q_type]
-print(prompt)
-
-# PREDICT CODE HERE
-print(predict_questions(prompt))
+if __name__ == '__main__':
+    app.run(debug = True, port = 2000)
