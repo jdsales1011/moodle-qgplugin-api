abb231a4597d48bab0a844900168d2fd8d98b3c0
jdsales1011 <jydeesales@gmail.com>
2023-05-17 03:22:53 +0800

    Revert endpoint

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 app = Flask(__name__)
-@app.route("/qgplugin/api/questions", methods = ["POST"])
+@app.route("/qgplugin/api/", methods = ["POST"])
 def get_questions():
-    print("INSIDE GET QUESTIONS!")
     # Get JSON parameter
     json_data = request.get_data(as_text=True)
 
